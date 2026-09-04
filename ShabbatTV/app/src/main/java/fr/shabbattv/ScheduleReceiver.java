003af8af6a10303ad923c8b986bfe43862cb5063
RKople <raphael@kople.net>
2026-09-04 06:45:22 -0700

    Add scheduled playback receiver

---
+++ ShabbatTV/app/src/main/java/fr/shabbattv/ScheduleReceiver.java
@@ -0,0 +1,25 @@
+package fr.shabbattv;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+import org.json.JSONObject;
+
+public class ScheduleReceiver extends BroadcastReceiver {
+    @Override public void onReceive(Context context, Intent intent) {
+        SharedPreferences p=AppState.prefs(context);
+        String movie=intent.getStringExtra("movie");
+        int volume=intent.getIntExtra("volume",AppState.defaultVolume(context));
+        p.edit().putLong("last_schedule_fire_at",System.currentTimeMillis()).apply();
+        try{
+            Intent play=new Intent(context,PlayerActivity.class);
+            play.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            play.putExtra("movie",movie);
+            play.putExtra("volume",volume);
+            play.putExtra("sleep_when_done",true);
+            context.startActivity(play);
+        }catch(Throwable t){p.edit().putString("last_schedule_error",t.toString()).apply();}
+    }
+}
