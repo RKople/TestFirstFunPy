df8c7c1e7f8fc9871667aa1b7e30a1d3116b7251
RKople <raphael@kople.net>
2026-09-04 06:43:09 -0700

    Prepare Shabbat TV 1.0 beta with Media3

--- ShabbatTV/app/build.gradle.kts
+++ ShabbatTV/app/build.gradle.kts
@@ -10,7 +10,12 @@ android {
         applicationId = "fr.shabbattv"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.4"
+        versionCode = 10
+        versionName = "1.0-beta"
     }
+}
+
+dependencies {
+    implementation("androidx.media3:media3-exoplayer:1.4.1")
+    implementation("androidx.media3:media3-ui:1.4.1")
 }
