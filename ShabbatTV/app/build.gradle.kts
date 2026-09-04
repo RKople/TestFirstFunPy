578476e60c61852567782a6296b80088ba5fa2f9
RKople <raphael@kople.net>
2026-09-04 07:54:36 -0700

    Bump Shabbat TV to v1.3

--- ShabbatTV/app/build.gradle.kts
+++ ShabbatTV/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "fr.shabbattv"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.2-beta"
+        versionCode = 13
+        versionName = "1.3-beta"
     }
 }
 
